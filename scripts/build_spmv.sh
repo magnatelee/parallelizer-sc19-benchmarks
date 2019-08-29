da5f89a28864cf54e299d67b0a3ff5f28d60cb3b
Wonchan Lee <wonchan@cs.stanford.edu>
2019-08-29 08:28:08 +0200

    Update build script for SpMV to collect compile time

--- scripts/build_spmv.sh
+++ scripts/build_spmv.sh
@@ -7,7 +7,7 @@ cd "$1"
 
 _GPU_ARCH="${GPU_ARCH:-pascal}"
 
-SAVEOBJ=1 OBJNAME=./spmv.auto "$ROOT_DIR"/../../regent.py "$ROOT_DIR"/../SpMV/Auto/spmv_sequential.rg -fflow 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch "$_GPU_ARCH"
+SAVEOBJ=1 OBJNAME=./spmv.auto time "$ROOT_DIR"/../../regent.py "$ROOT_DIR"/../SpMV/Auto/spmv_sequential.rg -fflow 0 -fcuda 1 -fcuda-offline 1 -fcuda-arch "$_GPU_ARCH" -flog 1 -fparallelize-use-colocation 0 |& tee compile_auto
 
 cp "$ROOT_DIR"/../../../bindings/regent/libregent.so .
 cp "$ROOT_DIR"/*_spmv*.sh .
